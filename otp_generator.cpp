ef54bf5752e54736c4e0db11694b421c5ad3be3d
Swati Aggrawal <swatiaggrawal86@gmail.com>
2022-06-16 19:47:59 +0530

    add new changes

--- otp_generator.cpp
+++ otp_generator.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
-void otpgenerator(int n){
+void otpgenerator(int n) //function to generate otp
+{
+    string str="0123456789"; 
+    int l=str.length();
+    string otp;
+    int j;
+    srand(time(0)); //to ensure each time a different otp is generated
     
 }
 int main(){
     int n;
     cout<<"Enter the length of the one time password";
-    cin>>n;
+    cin>>n; //input
     otpgenerator(n);
     return 0;
 }
