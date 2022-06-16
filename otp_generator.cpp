8d5985fa4fedc967da7a0d4768636f155e892d1a
Swati Aggrawal <swatiaggrawal86@gmail.com>
2022-06-16 19:31:42 +0530

    add changes

---
+++ otp_generator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void otpgenerator(int n){
+    
+}
+int main(){
+    int n;
+    cout<<"Enter the length of the one time password";
+    cin>>n;
+    otpgenerator(n);
+    return 0;
+}
