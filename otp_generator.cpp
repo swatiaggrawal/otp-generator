1c5bc87c60f124027d01361b724d930490e4212b
Swati Aggrawal <swatiaggrawal86@gmail.com>
2022-06-20 18:36:52 +0530

    add changes

--- otp_generator.cpp
+++ otp_generator.cpp
@@ -14,6 +14,7 @@ string otpgenerator(int n) //function to generate otp
         int index=rand()%l; //find index value between 0-9
         otp[i]=str[index]; //assign value to otp
     }
+    /*
     for(int i=0;i<n;i++){
         cout<<otp[i]; //display otp
     }
@@ -21,6 +22,8 @@ string otpgenerator(int n) //function to generate otp
     cout<<"OTP is going to expire in 10 seconds!!!!"<<endl;
     Sleep(10);
     cout<<"OTP is expired now!!!!!!!";
+    */
+    return otp;
     
 }
 int choiceoflength(int n){
@@ -31,22 +34,33 @@ int choiceoflength(int n){
 }
 int main(){
     int n;
-    int choice;
-    cout<<"Enter your choice!"<<endl;
-    cout<<"\t\tPress 1 for new OTP generation\n";
-    cout<<"\t\tPress 2 to exit\n";
-    cin>>choice;
-    switch (choice)
+    //int choice;
+    string finalotp;
+    char loopchoice;
+    do{
+    cout<<"Do you want to generate a new OTP?"<<endl;
+    cout<<"\t\tPress Y for new OTP generation\n";
+    cout<<"\t\tPress any key to exit\n";
+    cin>>loopchoice;
+    switch (loopchoice)
     {
-    case 1: system("cls");
-            n = choiceoflength(n);
-            cout<<"OTP generated is:"<<otpgenerator(n).c_str()<<endl;
-            Sleep(10);
-            break;
+    case 'Y':
+    case 'y':system("cls");
+             n = choiceoflength(n);
+             finalotp=otpgenerator(n);
+             cout<<"OTP generated is:"<<finalotp<<endl;
+             //Sleep(10);
+             cout<<endl;
+             cout<<"OTP is going to expire in 10 seconds!!!!"<<endl;
+             Sleep(10);
+             cout<<"OTP is expired now!!!!!!!";
+             break;
     
-    case 2: exit(0);
-            break;
+    default: exit(0);
+             break;
     }
+
+    }while(loopchoice=='y'|| loopchoice=='Y');
 
     return 0;
 }
